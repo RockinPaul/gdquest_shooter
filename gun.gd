05dadc43b01b7b608bd5a17ba7935b423021ad5c
Paul Zarudnev <rockinpaulz@gmail.com>
2025-08-15 12:43:43 +0200

    feat: implement shooting functionality in gun with bullet instantiation at shooting point

--- gun.gd
+++ gun.gd
@@ -1,7 +1,15 @@
 extends Area2D
 
+@onready var shooting_point: Marker2D = %ShootingPoint
+
 func _physics_process(delta: float) -> void:
 	var enemies_in_range = get_overlapping_bodies()
 	if enemies_in_range.size() > 0:
 		var target_enemy = enemies_in_range.front()
 		look_at(target_enemy.global_position)
+
+func shoot():
+	const BULLET = preload("res://bullet.tscn")
+	var new_bullet = BULLET.instantiate()
+	new_bullet.global_position = shooting_point
+	shooting_point.add_child(new_bullet)
