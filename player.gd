8cd343a19a43bf5add1df3c5fa6142379d222136
Paul Zarudnev <rockinpaulz@gmail.com>
2025-08-16 13:32:25 +0200

    feat: add player health system with damage from mob collisions

--- player.gd
+++ player.gd
@@ -1,6 +1,12 @@
 extends CharacterBody2D
 
+signal health_depleted
+
 @onready var happy_boo: Node2D = $HappyBoo
+@onready var hurt_box: Area2D = %HurtBox
+
+var health: float = 100.0
+const DAMAGE_RATE = 5.0
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -11,3 +17,12 @@ func _physics_process(delta: float) -> void:
 		$HappyBoo.play_walk_animation()
 	else:
 		$HappyBoo.play_idle_animation()
+		
+	var overlapping_mobs = hurt_box.get_overlapping_bodies()
+	var amount_of_overlapping_mobs = overlapping_mobs.size()
+	if amount_of_overlapping_mobs > 0:
+		health -= DAMAGE_RATE * amount_of_overlapping_mobs * delta
+		if health <= 0:
+			health_depleted.emit()
+			
+	
