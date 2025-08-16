d7ea80c5e2f0962848baac12c28a12a63e6e467b
Paul Zarudnev <rockinpaulz@gmail.com>
2025-08-16 14:28:12 +0200

    feat: add health progress bar to player with real-time updates

--- player.gd
+++ player.gd
@@ -4,6 +4,7 @@ signal health_depleted
 
 @onready var happy_boo: Node2D = $HappyBoo
 @onready var hurt_box: Area2D = %HurtBox
+@onready var progress_bar: ProgressBar = %ProgressBar
 
 var health: float = 100.0
 const DAMAGE_RATE = 5.0
@@ -22,6 +23,7 @@ func _physics_process(delta: float) -> void:
 	var amount_of_overlapping_mobs = overlapping_mobs.size()
 	if amount_of_overlapping_mobs > 0:
 		health -= DAMAGE_RATE * amount_of_overlapping_mobs * delta
+		progress_bar.value = health
 		if health <= 0:
 			health_depleted.emit()
 			
